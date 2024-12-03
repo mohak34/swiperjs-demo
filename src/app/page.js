d28a5e54ca3b3c677a4de5186d1c62f11d298cf2
mohak34 <mohaks0408@gmail.com>
2024-12-03 21:42:33 +0530

    changed js to jsx

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import SwiperComponent from "./components/SwiperComponent";
 export default function Home() {
   return (
     <main className="bg-[#FF5757] w-full min-h-screen flex justify-center items-center">
-      <SwiperComponent/>
+      <SwiperComponent />
     </main>
   );
 }
